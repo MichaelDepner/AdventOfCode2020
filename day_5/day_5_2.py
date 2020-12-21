3f222560f5033e48f7bab973fa644bae72177d83
Michael Frikke Madsen <mibias.madsen@gmail.com>
2020-12-21 11:00:45 +0100

    Improve day 5 part 2

--- day_5/day_5_2.py
+++ day_5/day_5_2.py
@@ -1,12 +1,19 @@
-from day_5_1 import get_highest_seat_id, get_seat
+from day_5_1 import get_seat
 
-def eliminate_ids(id_list):
+def get_all_seat_ids(lines):
+    seat_ids = []
+    for line in lines:
+        seat_ids.append(get_seat(line))
+    return seat_ids
+
+def eliminate_ids(id_list, range):
+    return set(range) - set(id_list)
+
+def run():
     with open('day_5/day_5.input') as f:
         lines = f.read().splitlines()
-    for line in lines:
-        seat_id = get_seat(line)
-        id_list.remove(seat_id)
-    return id_list
+    seat_ids = get_all_seat_ids(lines)
+    id_range = list(range(min(seat_ids),max(seat_ids)+1) )
+    print( eliminate_ids(seat_ids, id_range) )
 
-id_range = list( range(0,get_highest_seat_id()+1) )
-print( eliminate_ids(id_range) )
+run()
